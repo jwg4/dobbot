55368b8c74703d5d9648cb3801810375cdb81d52
Jack Grahl <jack.grahl@gmail.com>
2019-12-08 23:33:34 +0000

    Complete image-based tests

--- test/test_identification.py
+++ test/test_identification.py
@@ -1,6 +1,6 @@
 from nose.plugins.attrib import attr
 
-from dobbot.identification import image_to_play
+from dobbot.identification import image_to_symbol, card_to_list, image_to_play
 
 @attr(milestone=100)
 def test_image_to_play():
@@ -8,3 +8,28 @@ def test_image_to_play():
         data = f.read()
     value = image_to_play(data)
     self.assertEqual(value, "question mark")
+
+@attr(milestone=70)
+def test_image_to_play():
+    with open("img/single_card_001.jpg") as f:
+        data = f.read()
+    value = card_to_list(data)
+    expected = [
+        "yin yang",
+        "fire",
+        "orange man",
+        "bird",
+        "spider's web",
+        "heart",
+        "sunglasses",
+        "exclamation mark"
+    ]
+    self.assertEqual(value, expected)
+
+@attr(milestone=30)
+def test_image_to_play():
+    with open("img/single_symbol_001.jpg") as f:
+        data = f.read()
+    value = image_to_symbol(data)
+    self.assertEqual(value, "cheese")
+    
