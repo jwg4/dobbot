d1860a8b1f0f44e2e75e981d3188f9f23fd4bd96
Jack Grahl <jack.grahl@gmail.com>
2019-12-14 13:47:34 +0000

    Rename file.

--- test/test_identification.py
+++ test/test_identification.py
@@ -2,16 +2,18 @@ from nose.plugins.attrib import attr
 
 from dobbot.identification import image_to_symbol, card_to_list, image_to_play
 
+
 @attr(milestone=100)
 def test_image_to_play():
-    with open("img/two_cards_001.jpg") as f:
+    with open("test/img/two_cards_001.jpg") as f:
         data = f.read()
     value = image_to_play(data)
     self.assertEqual(value, "question mark")
 
+
 @attr(milestone=70)
 def test_image_to_play():
-    with open("img/single_card_001.jpg") as f:
+    with open("test/img/single_card_001.jpg") as f:
         data = f.read()
     value = card_to_list(data)
     expected = [
@@ -26,9 +28,10 @@ def test_image_to_play():
     ]
     self.assertEqual(value, expected)
 
+
 @attr(milestone=30)
 def test_image_to_play():
-    with open("img/single_symbol_001.jpg") as f:
+    with open("test/img/single_symbol__cheese__001.jpg", 'rb') as f:
         data = f.read()
     value = image_to_symbol(data)
     self.assertEqual(value, "cheese")
