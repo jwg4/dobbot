b0f0388ac2e0fb1e3e89cd0fdedc03e7d9fb4ade
Jack Grahl <jack.grahl@gmail.com>
2019-12-14 15:33:47 +0000

    Add two more tests.

--- test/test_logic.py
+++ test/test_logic.py
@@ -26,3 +26,61 @@ def test_one_common_object():
     ]
     value = sets_to_play(a, b)
     assert value == "yin yang", value
+
+
+@attr(milestone=5)
+def test_no_common_object():
+    a = [
+        "yin yang",
+        "fire",
+        "orange man",
+        "bird",
+        "spider's web",
+        "heart",
+        "sunglasses",
+        "exclamation mark",
+    ]
+    b = [
+        "ice cube",
+        "cheese",
+        "tree",
+        "stop sign",
+        "four leaf clover",
+        "question mark",
+        "ink blot",
+    ]
+    try:
+        value = sets_to_play(a, b)
+    except Exception as e:
+        assert e.message = ""
+        return
+    assert False, "No exception thrown."
+
+
+@attr(milestone=5)
+def test_two_common_objects():
+    a = [
+        "yin yang",
+        "fire",
+        "orange man",
+        "bird",
+        "spider's web",
+        "heart",
+        "sunglasses",
+        "exclamation mark",
+    ]
+    b = [
+        "ice cube",
+        "cheese",
+        "bird",
+        "stop sign",
+        "yin yang",
+        "question mark",
+        "ink blot",
+    ]
+    try:
+        value = sets_to_play(a, b)
+    except Exception as e:
+        assert e.message = ""
+        return
+    assert False, "No exception thrown."
