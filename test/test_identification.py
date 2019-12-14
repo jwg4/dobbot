78c1eea35b5b78346c42b4f4bc24f26667d50212
Jack Grahl <jack.grahl@gmail.com>
2019-12-14 14:00:09 +0000

    add error messages to asserts.

--- test/test_identification.py
+++ test/test_identification.py
@@ -8,8 +8,8 @@ def test_image_to_play():
     with open("test/img/two_cards_001.jpg") as f:
         data = f.read()
     value = image_to_play(data)
-    assert value == "question mark"
-
+    assert value == "question mark", value
+ 
 
 @attr(milestone=70)
 def test_image_to_play():
@@ -26,7 +26,7 @@ def test_image_to_play():
         "sunglasses",
         "exclamation mark"
     ]
-    assert value == expected
+    assert value == expected, value
 
 
 @attr(milestone=30)
@@ -34,5 +34,5 @@ def test_image_to_play():
     with open("test/img/single_symbol__cheese__001.jpg", 'rb') as f:
         data = f.read()
     value = image_to_symbol(data)
-    assert value == "cheese"
+    assert value == "cheese", value
     
